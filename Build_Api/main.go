7fdde011bd259ec01059b832f8b11c797a81f306
AkashNadar <Akashnadar14@gmail.com>
2022-12-29 23:57:40 +0530

    crud operation

--- Build_Api/main.go
+++ Build_Api/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // Models
@@ -24,11 +30,45 @@ var courses []Course
 
 // Middlewares
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
+	fmt.Println("starting...")
+	r := mux.NewRouter()
 
+	// data seeding
+	courses = append(courses, Course{
+		"2",
+		"ReactJs",
+		299,
+		&Author{
+			"Akash",
+			"akash.dev",
+		},
+	})
+
+	courses = append(courses, Course{
+		CourseId:    "3",
+		CourseName:  "Angular Js",
+		CoursePrice: 500,
+		Author: &Author{
+			Fullname: "Rahul",
+			Website:  "rahul.dev",
+		},
+	})
+
+	// Routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourse).Methods("GET")
+	r.HandleFunc("/courses/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/courses", addOneCourse).Methods("POST")
+	r.HandleFunc("/courses/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/courses/{id}", deleteOneCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // Controllers
@@ -40,4 +80,80 @@ func getAllCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("inside the getAllCourse controller")
 	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Inside the getOneCourse controller")
+	w.Header().Set("content-type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// find the course by id
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
+	return
+}
+
+func addOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Inside the addOneCourse controller")
+	w.Header().Set("content-type", "application/json")
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Inside the updateOneCourse controller")
+	w.Header().Set("content-type", "application/json")
+	// fmt.Println(r.Body)
+	params := mux.Vars(r)
+	var updateCourse Course
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			_ = json.NewDecoder(r.Body).Decode(&updateCourse)
+			course.CourseId = params["id"]
+			course.CourseName = updateCourse.CourseName
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("inside deleteOneCourse controller")
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			json.NewEncoder(w).Encode(course)
+			break
+		}
+	}
+
 }
---
+++ mongo_api/controller/controller.go
@@ -0,0 +1,169 @@
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"example.com/mongoapi/model"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://username:pass@cluster0.8weplsc.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// Most Important
+var collection *mongo.Collection
+
+// Connect with mongoDB
+func init() { //runs at the start but only one time
+	// client options
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	//connect to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB connection established")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+	fmt.Println("Collection instance is ready")
+}
+
+// Insert One Record
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one movie record :", inserted.InsertedID)
+}
+
+// Update One Record
+func updateOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count :", result.MatchedCount)
+}
+
+// Delete One Record
+func deleteOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie got deleted with delete count", deleteCount)
+
+}
+
+// delete all records from mongo
+func deleteAllMovie() int64 {
+	//filter := bson.D{{}} //every thing will be selected
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movies deleted :", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// Get all movies from mongodb
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+
+	}
+	defer cursor.Close(context.Background())
+	return movies
+}
+
+// the above codes are the db-helpers code which will be going in other folders
+
+// Actual controllers
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	res := deleteAllMovie()
+	json.NewEncoder(w).Encode(res)
+}
